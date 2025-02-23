6c30007d2a7e926d6b88b867e9a9fda1b2b15de1
Sahan Kalpana <shnkalpana@gmail.com>
2025-02-23 20:52:14 +0530

    edit css

--- resources/views/components/register_component.blade.php
+++ resources/views/components/register_component.blade.php
@@ -1,39 +1,40 @@
-<div class=" flex justify justify-center mt-5"> 
+<div class=" flex justify justify-center mt-5">
         <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700" >
             <form class=" space-y-6" action="/register" method="post">
                 @csrf
                 <h5 class="text-xl font-medium text-gray-900 dark:text-white">Create account</h5>
-                    
+
                         <div>
-                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> name: </label>
+                            <label class="block mb-1 text-sm font-medium text-gray-900 dark:text-white"> name: </label>
                             <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" type="text" name="name" placeholder="name" required />
                         </div>
                         <div>
-                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> email: </label>
+                            <label class="block mb-1 text-sm font-medium text-gray-900 dark:text-white"> email: </label>
                             <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" type="text" name="email" placeholder="email" required />
+                        </div>
                         <div class=" hidden">
-                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> User Roll: </label>
+                            <label class="block mb-1 text-sm font-medium text-gray-900 dark:text-white"> User Roll: </label>
                             <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" type="text" name="user_roll" placeholder="User Roll" />
                         </div>
                         <div>
-                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> dob: </label>
+                            <label class="block mb-1 text-sm font-medium text-gray-900 dark:text-white"> dob: </label>
                             <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" type="date" name="dob"/>
                         </div>
                         <div>
-                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> joined_date: </label>
+                            <label class="block mb-1 text-sm font-medium text-gray-900 dark:text-white"> joined_date: </label>
                             <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" type="date" name="joined_date">
-                            
+
                         </div>
                         <div class=" hidden">
-                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> hourly_rate: </label>
+                            <label class="block text-sm font-medium text-gray-900 dark:text-white"> hourly_rate: </label>
                             <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" type="text" name="hourly_rate" value="0.00" placeholder="hourly_rate">
                         </div>
                         <div>
-                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> password: </label>
+                            <label class="block mb-1 text-sm font-medium text-gray-900 dark:text-white"> password: </label>
                             <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" type="password" name="password" placeholder="password" required />
                         </div>
                         <div>
-                            <div class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <div class="w-full mb-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 <button >Register</button>
                             </div>
                         </div>
